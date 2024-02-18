fd332869a68af1d923c56f6a76670f1e5a3db806
manasvi102000 <150165248+manasvi102000@users.noreply.github.com>
2024-02-18 18:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let a=0,b=1,c,i;
+	if(n == 0)
+		return a;
+	for(i=2;i<=n;i++)
+		{
+		  c=a+b;
+		  a=b;
+		  b=c;	
+		}
+	return b;
 }
 
 module.exports = fibonacci;
